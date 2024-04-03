43492c63ad58d08a0935b6b7ff0ad32fb599ec71
Katlego Maredi <katlegomaredi9@gmail.com>
2024-04-03 15:15:18 +0200

    Lesson 6 second server

--- Unit_2/Lesson_6/server_html/main.js
+++ Unit_2/Lesson_6/server_html/main.js
@@ -1,3 +1,29 @@
+// const port = 3000,
+//     http = require("http"),
+//     httpStatus = require("http-status-codes"),
+//     fs = require("fs");
+// const routeMap = {
+//     "/": "views/index.html"
+// };
+// http.createServer((req, res) => {
+//     res.writeHead(httpStatus.OK, {
+//         "Content-Type": "text/html"
+//     });
+//     if (routeMap[req.url]) {
+//         fs.readFile(routeMap[req.url], (error, data) => {
+//             res.write(data);
+//             res.end();
+//         });
+//     }
+//     else {
+//         res.end("<h1>Sorry, not found.</h1>");
+//     }
+// })
+//     .listen(port);
+// console.log(`The server has started and is listening
+// ➥on port number: ${port}`);
+
+//Listing 6.3
 const port = 3000,
     http = require("http"),
     httpStatus = require("http-status-codes"),
@@ -5,20 +31,25 @@ const port = 3000,
 const routeMap = {
     "/": "views/index.html"
 };
+const getViewUrl = (url) => {
+    return `views${url}.html`;
+};
 http.createServer((req, res) => {
-    res.writeHead(httpStatus.OK, {
-        "Content-Type": "text/html"
-    });
-    if (routeMap[req.url]) {
-        fs.readFile(routeMap[req.url], (error, data) => {
+    let viewUrl = getViewUrl(req.url);
+    fs.readFile(viewUrl, (error, data) => {
+        if (error) {
+            res.writeHead(httpStatus.NOT_FOUND);
+            res.write("<h1>FILE NOT FOUND</h1>");
+        }
+        else {
+            res.writeHead(httpStatus.OK, {
+                "Content-Type": "text/html"
+            });
             res.write(data);
-            res.end();
-        });
-    }
-    else {
-        res.end("<h1>Sorry, not found.</h1>");
-    }
+        }
+        res.end();
+    });
 })
     .listen(port);
-console.log(`The server has started and is listening
-➥on port number: ${port}`);
+console.log(`The server has started and is listening on port number:
+    ➥${port}`);
